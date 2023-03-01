cd9aba96c72c78f1325f5b1b1d8d41f48a772ea3
Mariia-Olena <mariia.stus@gmail.com>
2023-03-01 20:07:40 +0200

    added header layout and not finished main landing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,63 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'flower-power';
+  navBar = [
+    {
+      name: 'shop',
+      link: '',
+    },
+    {
+      name: 'plant care',
+      link: '',
+    },
+    {
+      name: 'contact us',
+      link: '',
+    },
+    {
+      name: 'business gifting',
+      link: '',
+    },
+  ];
+
+  toolBar = [
+    {
+      name: 'log in',
+      iconLink: 'assets/icons/log_in.svg',
+    },
+    {
+      name: 'search',
+      iconLink: 'assets/icons/search.svg',
+    },
+    {
+      name: 'cart',
+      iconLink: 'assets/icons/cart.svg',
+    },
+  ];
+
+  plants = [
+    {
+      name: 'pilea peperomioides',
+      price: '36.99',
+      img: 'assets/plants/image_60.png',
+    },
+    {
+      name: 'scindapsus pictus',
+      price: '18.99',
+      img: 'assets/plants/image_105.png',
+    },
+    {
+      name: 'peace lily plant',
+      price: '29.99',
+      img: 'assets/plants/image_126.png',
+    },
+    {
+      name: 'monstera plant',
+      price: '42.99',
+      img: 'assets/plants/image_58.png',
+    },
+  ];
 }
