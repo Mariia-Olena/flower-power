b890b4c0310be23036d6c43ac59bc70a45c9e95b
Mariia-Olena <mariia.stus@gmail.com>
2023-03-23 13:21:19 +0200

    ls_3 add markup to NotFoundComponent

--- src/app/shop/shop-routing.module.ts
+++ src/app/shop/shop-routing.module.ts
@@ -4,14 +4,19 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProductsComponent } from './components/products/products.component';
 import { ProductComponent } from './components/product/product.component';
 import { ShopHomeComponent } from './components/shop-home/shop-home.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 const routes: Routes = [
   {
     path: '',
     component: ShopHomeComponent,
+
     children: [
-      { path: 'products/1', component: ProductComponent },
+      { path: 'products/:id', component: ProductComponent },
       { path: 'products', component: ProductsComponent },
+      { path: '', component: HomePageComponent },
+      { path: '**', component: NotFoundComponent },
     ],
   },
 ];
